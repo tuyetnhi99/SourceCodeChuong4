ea83f9103e323af358645563df91832c6ed00fa3
Your Name <you@example.com>
2019-10-17 23:27:20 +0700

    first commit

---
+++ FeedbackPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamlExamples
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FeedbackPage : ContentPage
+    {
+        const string placeholderText = "Type your message here";
+        public FeedbackPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
